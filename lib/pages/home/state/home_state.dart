241864dca7610b373fedeef0ecf025ee9a707f6d
NiwatFB <niwat.bua@factsblend.com>
2024-09-02 20:00:57 +0700

    ✨ feat: Use usecase on state ui

--- lib/pages/home/state/home_state.dart
+++ lib/pages/home/state/home_state.dart
@@ -1,10 +1,22 @@
 import 'package:get/get.dart';
+import 'package:subbeep/application/domain/Subscriptions_domain.dart';
+import 'package:subbeep/application/usecase/subscription/get_all_subscriptions_usecase.dart';
 
 class HomeState extends GetxController {
+  final getAllSubscriptionsUsecase = Get.find<GetAllSubscriptionsUsecase>();
+
+  final RxList<SubscriptionDomain> _subscriptionDomain =
+      <SubscriptionDomain>[].obs;
+  List<SubscriptionDomain> get subscriptions => _subscriptionDomain.toList();
+
   final _items = <Map<String, String>>[].obs;
   List<Map<String, String>> get items => _items;
 
   void setItems(List<Map<String, String>> items) {
     _items.value = items;
+  }
+
+  void loadSubscriptions() async {
+    _subscriptionDomain.addAll(await getAllSubscriptionsUsecase.call());
   }
 }
